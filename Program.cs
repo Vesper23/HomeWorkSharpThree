0de4f4f1310242d97503029e9437a10a44599a88
Valentin Likholat <phantom.231220@gmail.com>
2023-06-29 22:40:22 +0300

    Completed 1 task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+/*
+int reverse(int num, int acc)
+{
+    while (num > 0) 
+    { 
+        acc = acc * 10 + num % 10; num /= 10; 
+    }
+    return acc;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine((num == 0) || (reverse(num, 0) == num));
+Console.ReadKey(true);
+
+*/
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
