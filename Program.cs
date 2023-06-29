d41e04d2b2c8b73a5e31b884e17a97387ae26216
Valentin Likholat <phantom.231220@gmail.com>
2023-06-29 23:51:01 +0300

    Completed 3 Task

--- Program.cs
+++ Program.cs
@@ -49,3 +49,19 @@ Console.WriteLine(Math.Round(distance, 2));
 
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int num = InputNum("Введите число N: ");
+for(int i = 1; i <= num; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}");
+    if (i < num)
+    Console.Write(", ");
+    else
+    Console.Write(".");
+}
